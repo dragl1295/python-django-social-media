22a8788c37e72fa2a47cb72a9f40d995c2a9d3d7
dragl1295 <dhemantgoyal@gmail.com>
2024-08-09 13:05:27 +0530

    new

--- studybuddy/urls.py
+++ studybuddy/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("base.urls")),
     path('api/', include("base.api.urls")),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 
